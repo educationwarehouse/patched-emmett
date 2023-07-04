d51bb63eb1bc9d3664b66be9a44f0418c211c86f
SvenKeimpema <keimpje1234@gmail.com>
2023-07-04 15:53:36 +0200

    test

--- emmett/forms.py
+++ emmett/forms.py
@@ -651,6 +651,7 @@ class FormStyle:
             wtype = "float"
         try:
             if f"{wtype}" == "select":
+                raise Exception("HOE")
                 widget = self.widget_select(self.attr, field, value, _id=widget_id)
             else:
                widget = getattr(self, f"widget_{wtype}")(
